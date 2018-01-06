dfe968ed4be76ccd968717f4373fe72e1525a9dc
Rico A. Beti <rico.beti@silentbyte.com>
2018-01-06 22:42:02 +0800

    Extend AppBuilder

--- src/app.rs
+++ src/app.rs
@@ -81,27 +81,43 @@ pub struct Size {
 #[derive(Clone, Debug)]
 pub struct AppBuilder {
     title: String,
-    window_size: Size,
+    size: Option<Size>,
+    min_size: Option<Size>,
+    max_size: Option<Size>,
+    vsync: bool,
 }
 
 impl AppBuilder {
     pub fn new() -> Self {
         AppBuilder {
             title: "Sunlite".into(),
-            window_size: Size {
-                width: 1200,
-                height: 800,
-            },
+            size: None,
+            max_size: None,
+            min_size: None,
+            vsync: false,
         }
     }
 
     pub fn build(&self) -> App {
         let events_loop = glutin::EventsLoop::new();
-        let window_builder = glutin::WindowBuilder::new()
-            .with_title(self.title.clone())
-            .with_dimensions(self.window_size.width, self.window_size.height);
+        let mut window_builder = glutin::WindowBuilder::new()
+            .with_title(self.title.clone());
 
-        let context_builder = glutin::ContextBuilder::new();
+        if let Some(ref s) = self.size {
+            window_builder = window_builder.with_dimensions(s.width, s.height);
+        }
+
+        if let Some(ref s) = self.min_size {
+            window_builder = window_builder.with_min_dimensions(s.width, s.height);
+        }
+
+        if let Some(ref s) = self.max_size {
+            window_builder = window_builder.with_max_dimensions(s.width, s.height);
+        }
+
+        let context_builder = glutin::ContextBuilder::new()
+            .with_vsync(self.vsync);
+
         let (window, device, mut factory, main_fbo, main_dbo) =
             gfx_window_glutin::init::<ColorFormat, DepthFormat>
                 (window_builder, context_builder, &events_loop);
@@ -117,5 +133,30 @@ impl AppBuilder {
             gfx_main_dbo: main_dbo,
             gfx_cmd_queue: cmd_queue,
         }
+    }
+
+    pub fn title<S: Into<String>>(&mut self, title: S) -> &mut AppBuilder {
+        self.title = title.into();
+        self
+    }
+
+    pub fn size(&mut self, width: u32, height: u32) -> &mut AppBuilder {
+        self.size = Some(Size { width, height });
+        self
+    }
+
+    pub fn min_size(&mut self, width: u32, height: u32) -> &mut AppBuilder {
+        self.min_size = Some(Size { width, height });
+        self
+    }
+
+    pub fn max_size(&mut self, width: u32, height: u32) -> &mut AppBuilder {
+        self.max_size = Some(Size { width, height });
+        self
+    }
+
+    pub fn vsync(&mut self, vsync: bool) -> &mut AppBuilder {
+        self.vsync = vsync;
+        self
     }
 }
